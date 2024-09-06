208d575a1b8685a61c644ad8830dbae856d90240
loongbatian <galh3013@gmail.com>
2024-09-06 06:46:59 +0500

    客户端页面初始化

--- luffycityweb/src/App.vue
+++ luffycityweb/src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <router-view></router-view>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* 声明全局样式和项目的初始化样式 */
+body,h1,h2,h3,h4,p,table,tr,td,ul,li,a,form,input,select,option,textarea{
+  margin:0;
+  padding: 0;
+  font-size: 15px;
+}
+a{
+  text-decoration: none;
+  color: #333;
+  cursor: pointer;
+}
+ul,li{
+  list-style: none;
+}
+table{
+  border-collapse: collapse; /* 合并边框 */
+}
+img{
+  max-width: 100%;
+  max-height: 100%;
+}
+input{
+  outline: none;
 }
 </style>
