b3dc84c9a58fef8c06087c4d51ac0e0d5a101d45
loongbatian <galh3013@gmail.com>
2024-09-13 13:47:20 +0500

    feature:实现jwt认证和用户登入前后端交互

--- luffycityapi/luffycityapi/settings/dev.py
+++ luffycityapi/luffycityapi/settings/dev.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 import sys
 
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'corsheaders',
+    'rest_framework_simplejwt',
 
     'luffycityapi.luffycityapi.apps.home',
     'luffycityapi.luffycityapi.apps.users',
@@ -51,6 +53,12 @@ EXCEPTION_HANDLER_MODULE = str(BASE_DIR / "utils")
 REST_FRAMEWORK = {
     # 自定义异常处理
     'EXCEPTION_HANDLER': 'luffycityapi.luffycityapi.utils.exceptions.custom_exception_handler',
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
 
 }
 
@@ -266,3 +274,33 @@ MEDIA_URL = "/uploads/"
 
 # 认证模型,这是app名称和模型名称
 AUTH_USER_MODEL = 'users.User'
+
+from django.conf import settings
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': settings.SECRET_KEY,
+    'VERIFYING_KEY': '',
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JSON_ENCODER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+    'JTI_CLAIM': 'jti',
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
--- luffycityweb/src/main.js
+++ luffycityweb/src/main.js
@@ -1,12 +1,10 @@
 import {createApp} from 'vue'
 import App from './views/App.vue'
 import router from "./router/index";
-import ElementPlus from 'element-plus' //全局引入
+// import ElementPlus from 'element-plus' //全局引入
 import 'element-plus/dist/index.css'; // 引入 Element Plus 的样式
-createApp(App).mount('#app')
 
 
 createApp(App)
     .use(router)  // 使用 router
-    .use(ElementPlus)
     .mount('#app')
